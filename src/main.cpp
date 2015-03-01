066510f477db37697173bd212fdc9a0362b3e473
Michael Christensen-Calvin <mrcc912@gmail.com>
2015-03-01 10:38:18 -0800

    super basic c++ project template with sdl setup

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "SDL/SDL.h"
+
+int main(int argc, char* argv[])
+{
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  SDL_Surface* hello = NULL;
+  SDL_Surface* screen = NULL;
+  SDL_Quit();
+  return 0;
+}
